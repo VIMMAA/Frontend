51cf11cef8cc583b21372ad56fbaffd3fbe29f91
ruvich <alaptev1308@gmail.com>
2025-03-12 23:46:25 +0700

    функция рендера заявок

--- js/applicationsList.js
+++ js/applicationsList.js
@@ -56,3 +56,65 @@ function updateNavbar() {
 }
 
 updateNavbar();
+
+function renderApplications(app) {
+    const createAppliacationButton = document.querySelector('.createAppliacationButton');
+    const applicationNum = document.querySelector('.applicationNum');
+
+    const makeDecision = document.querySelector('.makeDecision');
+    const acceptButton = document.querySelector('.acceptButton');
+    const rejectButton = document.querySelector('.rejectButton');
+
+    const applicationStatus = document.querySelector('.applicationStatus');
+    const acceptedApplication = document.querySelector('.acceptedApplication');
+    const rejectedApplication = document.querySelector('.rejectedApplication');
+    const editApplication = document.querySelector('.editApplication');
+    const reviewApplication = document.querySelector('.reviewApplication');
+
+    const applicationDate = document.querySelector('.applicationDate');
+
+    const applicationContainer = document.getElementById('applicationContainer');
+    const applicationTemplate = document.getElementById('applicationTemplate');
+    const applicationClone = applicationTemplate.contentEditable.cloneNode(true);
+
+
+    applicationNum.textContent = app.id;
+    applicationDate.textContent = new Date(app.applicationDate).toLocaleDateString();
+
+    //NotDefined Approved Declined - статусы заявки
+    //Student Teacher Dean Admin   - роли пользователей
+
+    if (userRole === "Student") { 
+        applicationStatus.classList.remove("d-none");
+        createAppliacationButton.classList.remove("d-none");
+        if (app.status === "NotDefined") {
+            editApplication.classList.remove("d-none");
+            reviewApplication.classList.remove("d-none");
+        } else if (app.status === "Approved") {
+            acceptedApplication.classList.remove("d-none");
+        } else if (app.status === "Declined") {
+            rejectedApplication.classList.remove("d-none");
+        }
+    } else if (userRole === "Teacher") {
+        applicationStatus.classList.remove("d-none");
+        if (app.status === "NotDefined") {
+            reviewApplication.classList.remove("d-none");
+        } else if (app.status === "Approved") {
+            acceptedApplication.classList.remove("d-none");
+        } else if (app.status === "Declined") {
+            rejectedApplication.classList.remove("d-none");
+        }
+    } else if (userRole === "Dean" || userRole === "Admin") {
+        if (app.status === "NotDefined") {
+            makeDecision.classList.remove("d-none");
+            acceptButton.classList.remove("d-none");
+            rejectButton.classList.remove("d-none");
+        } else if (app.status === "Approved") {
+            acceptedApplication.classList.remove("d-none");
+        } else if (app.status === "Declined") {
+            rejectedApplication.classList.remove("d-none");
+        }
+    }
+
+    applicationContainer.appendChild(applicationClone);
+}

f7cef41a0f444464bade67542deb86f76fe61746
G3nKai <kulikovila2005@gmail.com>
2025-03-14 01:08:58 +0700

    смена календаря на расписание и наоборот

--- js/schedule.js
+++ js/schedule.js
@@ -1,38 +1,52 @@
 
 //необходимо грамотно для расписания переменные подсчитать потом
+//нужно помимо таблиц еще вывод дней недели поменять
+let scheduleStatement = document.querySelectorAll('table');
+console.log(scheduleStatement);
+
+let changeStatement = document.querySelector('#statement');
+console.log(changeStatement);
+
 let tempCells = document.querySelectorAll("td");
-let cells = Array.from(tempCells);
+    let cells = Array.from(tempCells);
+    
+    cells = scheduleStatement[0].classList.contains('d-none') ? Array.from(tempCells).filter((_, index) => (index) % 7 !== 0) : Array.from(tempCells);
+    
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            if (cell.textContent.length > 0)
+                    cell.classList.toggle("selected");
+            else if (cell.classList.contains('selected')) {
+                    cell.classList.toggle("selected");
+            }
+        })
+    });
 
-cells = Array.from(tempCells).filter((_, index) => (index) % 7 !== 0)
-
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        if (cell.textContent.length > 0)
-                cell.classList.toggle("selected");
-        else if (cell.classList.contains('selected')) {
-                cell.classList.toggle("selected");
+changeStatement.addEventListener('click', () => {
+    scheduleStatement.forEach(statement => {
+        if (!statement.classList.contains('d-none')) {
+            statement.classList.toggle('d-none');
+        } else {
+            statement.classList.toggle('d-none');
         }
+        
     })
+
+    let tempCells = document.querySelectorAll("td");
+    let cells = Array.from(tempCells);
+    
+    cells = scheduleStatement[0].classList.contains('d-none') ? Array.from(tempCells).filter((_, index) => (index) % 7 !== 0) : Array.from(tempCells);
+    
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            if (cell.textContent.length > 0)
+                    cell.classList.toggle("selected");
+            else if (cell.classList.contains('selected')) {
+                    cell.classList.toggle("selected");
+            }
+        })
+    });
 });
-
-function highlightCells(firstCell, lastCell) {
-    const firstIndex = Array.from(firstCell.parentElement.children).indexOf(firstCell);
-    const lastIndex = Array.from(lastCell.parentElement.children).indexOf(lastCell);
-
-    document.querySelectorAll('.selected').forEach(cell => {
-        cell.classList.remove('selected');
-    });
-
-    const start = Math.min(firstIndex, lastIndex);
-    const end = Math.max(firstIndex, lastIndex);
-
-    rows.forEach(row => {
-        for (let i = start; i <= end; i++) {
-            if (row.children[i].textContent.length > 0)
-                row.children[i].classList.add("selected");
-        }
-    });
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     const fileInput = document.getElementById('fileInput');
